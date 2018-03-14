7628cdf3c7a3051009554d6a314885ccc2bb7adb
Nicholas Bergesen <nicholasb.za@gmail.com>
2018-03-14 23:26:47 +0200

    First commit

---
+++ PollyConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PollyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oh hi mark");
+            Console.ReadLine();
+        }
+    }
+}
