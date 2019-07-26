6608514b8c51efb737fba795d47e007ad401a86e
WGqIbSQpswJlIN901DMF <brucesalingre@gmail.com>
2019-07-26 18:33:36 +0200

    Moved graph to typescript

---
+++ Polly.Website/Views/Home/_PriceHistoryGraph.ts
@@ -0,0 +1,48 @@
+﻿namespace Polly {
+
+    export class PriceHistoryGraph {
+
+        constructor(options: Options) {
+
+            var ctx = <HTMLCanvasElement>document.getElementById("myChart");
+
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: options.labels,
+                    datasets: [{
+                        label: 'Price',
+                        data: options.data,
+                        backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+                        borderColor: ['rgba(255,99,132,1)'],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    hover: {
+                        mode: 'nearest',
+                    },
+                    scales: {
+                        xAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                labelString: 'Date'
+                            }
+                        }],
+                        yAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                labelString: 'Price'
+                            }
+                        }]
+                    }
+                }
+            });
+        }
+    }
+
+    interface Options {
+        data: Array<number>;
+        labels: Array<string>;
+    }
+}
