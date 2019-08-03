2815f0287d0081d7535172e8239085153dd74e6e
Nicholas Bergesen <nicholasb.za@gmail.com>
2019-08-03 13:19:05 +0200

    Cleaned up after merge

--- Polly.Website/Models/DealModel.cs
+++ Polly.Website/Models/DealModel.cs
@@ -1,9 +1,8 @@
-﻿using Polly.Data;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Web;
 
 namespace Polly.Website.Models
 {
