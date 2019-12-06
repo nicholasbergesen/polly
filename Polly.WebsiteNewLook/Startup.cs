acea9dee1793a57afb1b77a0280c3f62c51c6169
Nicholas Bergesen <nicholasb.za@gmail.com>
2019-12-06 21:43:29 +0200

    Added new styling project

---
+++ Polly.WebsiteNewLook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Polly.WebsiteNewLook.Startup))]
+namespace Polly.WebsiteNewLook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
