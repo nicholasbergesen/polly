9e363eaaf143a5f58f162f62f0e125b8a9d5f978
Nicholas Bergesen <nicholasb@property24.com>
2019-12-09 18:37:46 +0200

    Added migration for new tables

--- Polly.Data/Models/IdentityClasses.cs
+++ Polly.Data/Models/IdentityClasses.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
-using System.Text;
 using System.Threading.Tasks;
-using System.Data.Entity;
 
 namespace Polly.Data
 {
@@ -31,6 +26,7 @@ namespace Polly.Data
 
         public bool IsEnabled { get; set; }
     }
+
 
     public class UserRole : IdentityUserRole<long>
     {
